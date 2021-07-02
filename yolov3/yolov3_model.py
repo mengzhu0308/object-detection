75776edfb94a8bcc732a96da88e89eac124ab628
Meng <75422140+mengzhu0308@users.noreply.github.com>
2021-07-02 12:29:36 +0800

    Update yolov3_model.py

--- yolov3/yolov3_model.py
+++ yolov3/yolov3_model.py
@@ -88,7 +88,7 @@ def yolo_head(feats, anchors, model_input_shape):
 
 def box_iou(b1, b2):
     # Expand dim to apply broadcasting.
-    b1 = K.expand_dims(b1, -2)
+    b1 = b1[:, None, :]
     b1_xy = b1[..., :2]
     b1_wh = b1[..., 2:4]
     b1_wh_half = b1_wh / 2.
@@ -96,12 +96,12 @@ def box_iou(b1, b2):
     b1_maxes = b1_xy + b1_wh_half
 
     # Expand dim to apply broadcasting.
-    b2 = K.expand_dims(b2, 0)
-    b1_xy = b2[..., :2]
+    b2 = b2[None, :, :]
+    b2_xy = b2[..., :2]
     b2_wh = b2[..., 2:4]
     b2_wh_half = b2_wh / 2.
-    b2_mins = b1_xy - b2_wh_half
-    b2_maxes = b1_xy + b2_wh_half
+    b2_mins = b2_xy - b2_wh_half
+    b2_maxes = b2_xy + b2_wh_half
 
     intersect_mins = K.maximum(b1_mins, b2_mins)
     intersect_maxes = K.minimum(b1_maxes, b2_maxes)
