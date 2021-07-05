e65ff3c4ef854950dcef41e64ff04fd413824ea8
Meng <75422140+mengzhu0308@users.noreply.github.com>
2021-07-05 09:52:58 +0800

    Update CollateFn.py

--- yolov3_pt/CollateFn.py
+++ yolov3_pt/CollateFn.py
@@ -25,14 +25,15 @@ class CollateFn:
                  anchors,
                  model_input_shape=Config.MODEL_INPUT_SHAPE,
                  anchor_mask=Config.ANCHOR_MASK,
-                 downsampling_scale=Config.DOWNSAMPLING_SCALE,
+                 downsampling_scales=Config.DOWNSAMPLING_SCALES,
                  ignore_threshold=0.5):
         self.num_classes = num_classes
         self.model_input_shape = np.array(model_input_shape)
         self.anchors_per_location = [anchors[am] for am in anchor_mask]
         self.num_anchors_per_location = len(anchor_mask[0])
         self.num_layers = len(anchor_mask)
-        self.feat_shapes = [self.model_input_shape // ds for ds in downsampling_scale]
+        self.feat_shapes = [np.ceil(self.model_input_shape / ds) for ds in downsampling_scales]
+        self.downsampling_scales = downsampling_scales
         self.ignore_threshold = ignore_threshold
 
     def __call__(self, data):
@@ -43,14 +44,14 @@ class CollateFn:
 
         batch_true_bboxes = [t[-1] for t in data]
         targets = [
-            np.zeros(shape=(batch_size, self.num_classes + 5, self.num_anchors_per_location, h, w), dtype='float32')
+            np.zeros(shape=(batch_size, h, w, self.num_anchors_per_location, self.num_classes + 5), dtype='float32')
             for h, w in self.feat_shapes]
         bbox_loss_scales = []
         bbox_loss_scales_mem = [
-            np.zeros(shape=(batch_size, 2, self.num_anchors_per_location, h, w), dtype='float32')
+            np.zeros(shape=(batch_size, h, w, self.num_anchors_per_location, 2), dtype='float32')
             for h, w in self.feat_shapes]
         ignore_masks = [
-            np.zeros(shape=(batch_size, 1, self.num_anchors_per_location, h, w), dtype='float32')
+            np.zeros(shape=(batch_size, h, w, self.num_anchors_per_location, 1), dtype='float32')
             for h, w in self.feat_shapes]
 
         for l in range(self.num_layers):
@@ -59,29 +60,28 @@ class CollateFn:
             ignore_mask = ignore_masks[l]
             for b in range(batch_size):
                 true_bboxes = batch_true_bboxes[b]
-                for t in range(len(true_bboxes)):
-                    xy = (true_bboxes[t][0:2] + true_bboxes[t][2:4]) / 2
-                    wh = true_bboxes[t][2:4] - true_bboxes[t][0:2]
-
-                    xy = xy / self.model_input_shape[::-1] * self.feat_shapes[l][::-1]
+                for true_bbox in true_bboxes:
+                    xy = (true_bbox[0:2] + true_bbox[2:4]) / 2
+                    wh = true_bbox[2:4] - true_bbox[0:2]
+                    xy = xy / self.downsampling_scales[l]
                     ij = xy.astype('int32')
 
                     bbox_shape = np.array([0, 0, wh[0], wh[1]], dtype='float32')[None, :]
                     anchor_shapes = np.concatenate([np.zeros((self.num_anchors_per_location, 2), dtype='float32'),
                                                     self.anchors_per_location[l].astype('float32')], axis=1)
-                    iou_cur = bbox_iou(bbox_shape, anchor_shapes)
-                    best_n = np.argmax(iou_cur)
+                    iou = bbox_iou(bbox_shape, anchor_shapes)
+                    best_n = np.argmax(iou)
 
-                    target[b, 0:2, best_n, ij[1], ij[0]] = xy - ij
-                    target[b, 2:4, best_n, ij[1], ij[0]] = np.log(wh / self.anchors_per_location[l][best_n] + 1e-12)
-                    target[b, 4, best_n, ij[1], ij[0]] = 1
-                    target[b, int(true_bboxes[t][-1]), best_n, ij[1], ij[0]] = 1
+                    target[b, ij[1], ij[0], best_n, 0:2] = xy - ij
+                    target[b, ij[1], ij[0], best_n, 2:4] = np.log(wh / self.anchors_per_location[l][best_n])
+                    target[b, ij[1], ij[0], best_n, 4] = 1
+                    target[b, ij[1], ij[0], best_n, int(true_bbox[-1])] = 1
 
-                    bbox_loss_scale_mem[b, :, best_n, ij[1], ij[0]] = wh / self.model_input_shape[::-1]
+                    bbox_loss_scale_mem[b, ij[1], ij[0], best_n, :] = wh / self.model_input_shape[::-1]
 
-                    ignore_mask[b, 0, iou_cur > self.ignore_threshold, ij[1], ij[0]] = 0
+                    ignore_mask[b, ij[1], ij[0], iou > self.ignore_threshold, 0] = 0
 
-            bbox_loss_scales.append(2 - bbox_loss_scale_mem[:, 0:1, ...] * bbox_loss_scale_mem[:, 1:2, ...])
+            bbox_loss_scales.append(2 - bbox_loss_scale_mem[..., 0:1] * bbox_loss_scale_mem[..., 1:2])
 
         targets = [torch.from_numpy(e) for e in targets]
         bbox_loss_scales = [torch.from_numpy(e) for e in bbox_loss_scales]
